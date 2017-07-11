8d23a3186b5e412f48828692d3acc63705a32f70
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-11 17:35:46 -0400

    Added SuperHuman Class

---
+++ Comp123-lesson2b/SuperHuman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name:Dennis Kanzira
+ * Date:July 11 2017
+ * Description: This is the SuperHuman class that extends the Human abstract class
+ * version: 0.1 - Created the Program Class
+ * 
+*/
+namespace Comp123_lesson2b
+{
+    /// <summary>
+    /// This is the Super Human class
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        //PRIVATE INSTANCE VARIABLES(FIELDS)
+
+        //PUBLIC PROPERITIES
+
+        //CONSTRUCTORS -----------------------------------------------------
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
