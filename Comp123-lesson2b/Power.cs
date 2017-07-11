ab5819d7441fd086b312bf3465bbbfd9c1dd58c4
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-11 17:48:47 -0400

    Added the Power Class

---
+++ Comp123-lesson2b/Power.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name:Dennis Kanzira
+ * Date:July 11 2017
+ * Description: This is the Power Class
+ * version: 0.1 - Created the Power class Class
+ * 
+*/
+namespace Comp123_lesson2b
+{
+    /// <summary>
+    /// This is the Power class which will be used as a data type
+    /// </summary>
+    public class Power
+    {
+        //PUBLIC PROPERTIES
+        public string Name { get; set; }
+
+        public int Rank { get; set; }
+
+
+    }
+}
--- Comp123-lesson2b/SuperHuman.cs
+++ Comp123-lesson2b/SuperHuman.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name:Dennis Kanzira
  * Date:July 11 2017
  * Description: This is the SuperHuman class that extends the Human abstract class
- * version: 0.1 - Created the Program Class
+ * version: 0.2 - Added _initialize method
  * 
 */
 namespace Comp123_lesson2b
@@ -18,17 +18,29 @@ namespace Comp123_lesson2b
     public class SuperHuman : Human
     {
         //PRIVATE INSTANCE VARIABLES(FIELDS)
-
+        private List<Power> _powers;
         //PUBLIC PROPERITIES
 
         //CONSTRUCTORS -----------------------------------------------------
+        /// <summary>
+        /// This is the main constructor for SuperHuman Class
+        /// It takes one parameter - name(string)
+        /// </summary>
+        /// <param name="name"></param>
         public SuperHuman(string name)
             :base(name)
         {
-
+            this._intialize();
         }
 
         //PRIVATE METHODS
+        /// <summary>
+        /// This method initializes, instantiates ans ass
+        /// </summary>
+        private void _intialize()
+        {
+            this._powers = new List<Power>();//Creates an empty list
+        }
 
         //PUBLIC METHODS
     }
