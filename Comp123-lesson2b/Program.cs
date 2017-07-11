6af0215e517d45df739fb6a4223220fd33c5eb87
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-11 17:11:00 -0400

    Add project files.

---
+++ Comp123-lesson2b/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_lesson2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
