38564ade0d72c6a74dc741118b096b023bae94a1
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-11 17:29:21 -0400

    Added Human Abstract class

---
+++ Comp123-lesson2b/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name:Dennis Kanzira
+ * Date:July 11 2017
+ * Description: This is the Abstract Human class
+ * version: 0.1 - Created the Program Class
+ * 
+*/
+namespace Comp123_lesson2b
+{
+    /// <summary>
+    /// This is the Abstract Human Class
+    /// </summary>
+    public abstract class Human
+    {
+        //PUBLIC INSTANCE VARIABLES(FIELDS)
+        private string _name;
+
+        //PUBLIC PROPERITIES
+        public string Name
+        {
+
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+                
+        /// <summary>
+        /// This is the main constructor for the Human class
+        /// It takes the paramenter - name(string)
+        /// </summary>
+        /// <param name="name"></param>
+        //CONSTRUCTORS -----------------------------------------------------
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+
+        //PRIVATE METHODS
+
+        //PUBLIC METHODS
+    }
+}
--- Comp123-lesson2b/Program.cs
+++ Comp123-lesson2b/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 /*
  * Name:Dennis Kanzira
- * Date:July
+ * Date:July 11 2017
+ * Description: Demmo for lesson 9
+ * version: 0.1 - Created the Program Class
  * 
- * /
- * 
+*/
+
 namespace Comp123_lesson2b
 {
     class Program
